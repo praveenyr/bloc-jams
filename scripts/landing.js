0c9f96aa178cbefe8952d4466aa473f1d7937cd5
Praveen Yallakara <praveenyr@gmail.com>
2016-08-25 23:11:13 -0700

    Complete Assignment-9-DOM-Events

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,23 +1,23 @@
 var pointsArray = document.getElementsByClassName('point');
 
+var revealPoint = function(point){
+               //for(var i=0; i<points.length; i++) {
+             point.style.opacity = 1;
+             point.style.transform = "scaleX(1) translateY(0)";
+             point.style.msTransform = "scaleX(1) translateY(0)";
+            point.style.WebkitTransform = "scaleX(1) translateY(0)";
+      //  }
+  };
 var animatePoints = function(points) {
+    forEach(points, revealPoint);
 
-        var revealPoint = function(){
-             for(var i=0; i<points.length; i++) {
-                   points[i].style.opacity = 1;
-                   points[i].style.transform = "scaleX(1) translateY(0)";
-                   points[i].style.msTransform = "scaleX(1) translateY(0)";
-                   points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-              }
-        };
-        revealPoint();
-     };
+   };
 
         window.onload = function(){
 
           if (window.innerHeight > 950) {
               animatePoints(pointsArray);
-          }
+            }
           var sellingPoints = document.getElementsByClassName('selling-points')[0];
           var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
