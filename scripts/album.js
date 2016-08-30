4a9610eafebee50f8defff7b32b7515d8b42bf19
Praveen Yallakara <praveenyr@gmail.com>
2016-08-30 10:21:33 -0700

    Complete Assignment-11-DOM-AlbumView

--- scripts/album.js
+++ scripts/album.js
@@ -29,6 +29,22 @@ var albumMarconi = {
     ]
 };
 
+//Third Album for Assigment in Checkpoint-10 DOM Scripting - Album View
+var albumBackInBlack = {
+    title: 'Back In Black',
+    artist: 'AC/DC',
+    label: 'EMI Records',
+    year: '1980',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    //albumArtUrl: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=0ahUKEwjgvamstenOAhUG_WMKHaOMA-YQjRwIBw&url=http%3A%2F%2Fwww.listal.com%2Flist%2Ffavorite-album-covers-1980s&psig=AFQjCNE8hcA-7TDnKZ7H3ROFVf0nBGQhYQ&ust=1472656020065868';
+    songs: [
+        { title: 'Hells Bells', duration: '5:12' },
+        { title: 'What Do You Do For Money Honey?', duration: '3:35' },
+        { title: 'Back In Black', duration: '4:15'},
+        { title: 'Rock N Roll Aint Noise Pollution', duration: '4:15' },
+        { title: 'Shoot To Thrill', duration: '5:18'}
+    ]
+};
 
  var createSongRow = function(songNumber, songName, songLength) {
      var template =
@@ -42,13 +58,15 @@ var albumMarconi = {
      return template;
  };
 
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
  var setCurrentAlbum = function(album) {
      // #1
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
-     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-     var albumImage = document.getElementsByClassName('album-cover-art')[0];
-     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
 
      // #2
      albumTitle.firstChild.nodeValue = album.title;
@@ -65,6 +83,17 @@ var albumMarconi = {
      }
  };
 
+
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
+
+     var albumArr = [albumBackInBlack,albumMarconi,albumPicasso];
+     var i = 0;
+     albumImage.addEventListener("click",function(event){
+          setCurrentAlbum(albumArr[i]);
+          i++;
+          if(i == albumArr.length){
+            i = 0;
+          }
+     });
  };
