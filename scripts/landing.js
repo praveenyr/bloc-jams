03267b89277ad23cfbb36e1012e9a27033c05342
Praveen Yallakara <praveenyr@gmail.com>
2016-09-07 21:56:17 -0700

    Complete Checkpoint-16-jQuery-LandingPage

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,31 +1,41 @@
-var pointsArray = document.getElementsByClassName('point');
+// var pointsArray = document.getElementsByClassName('point');
+//
+// var revealPoint = function(point){
+//                //for(var i=0; i<points.length; i++) {
+//              point.style.opacity = 1;
+//              point.style.transform = "scaleX(1) translateY(0)";
+//              point.style.msTransform = "scaleX(1) translateY(0)";
+//             point.style.WebkitTransform = "scaleX(1) translateY(0)";
+//       //  }
+//   };
+// var animatePoints = function(points) {
+//     forEach(points, revealPoint);
+//
+//    };
 
-var revealPoint = function(point){
-               //for(var i=0; i<points.length; i++) {
-             point.style.opacity = 1;
-             point.style.transform = "scaleX(1) translateY(0)";
-             point.style.msTransform = "scaleX(1) translateY(0)";
-            point.style.WebkitTransform = "scaleX(1) translateY(0)";
-      //  }
-  };
-var animatePoints = function(points) {
-    forEach(points, revealPoint);
+ var animatePoints = function() {
+     var revealPoint = function() {
+       $(this).css({
+          opacity: 1,
+          transform: 'scaleX(1) translateY(0)'
+      });
+    };
+    $.each($('.point'), revealPoint);
+};
 
-   };
+        $(window).load(function(){
 
-        window.onload = function(){
-
-          if (window.innerHeight > 950) {
-              animatePoints(pointsArray);
+          if ($(window).height > 950) {
+              animatePoints();
             }
-          var sellingPoints = document.getElementsByClassName('selling-points')[0];
-          var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+          // var sellingPoints = document.getElementsByClassName('selling-points')[0];
+          // var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+            var scrollDistance = $(sellingPoints).offset().top - $(window).height() + 200;
 
-
-          window.addEventListener('scroll', function(event) {
+          $(window).scroll(function(event) {
           //  console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
-            if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-             animatePoints(pointsArray);
+          if ($(window).scrollTop() >= scrollDistance) {
+              animatePoints();
          }
         });
-    };
+    });
