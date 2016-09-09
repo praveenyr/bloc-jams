70e8c492e5af3194c79145e4f344b2390a15a640
Praveen Yallakara <praveenyr@gmail.com>
2016-09-09 09:53:49 -0700

    Complete Checkpoint-17-jquery-CollandAlbumViews

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,11 +1,12 @@
-var collectionItemTemplate =
+var buildCollectionItemTemplate = function(index) {
+    var template =
     '<div class="collection-album-container column fourth">'
   + '  <img src="assets/images/album_covers/01.png"/>'
   + '  <div class="collection-album-info caption">'
   + '    <p>'
   + '      <a class="album-name" href="/album.html"> The Colors </a>'
   + '      <br/>'
-  + '      <a href="/album.html"> Pablo Picasso </a>'
+  + '      <a href="/album.html">Pablo Picasso#' + [index+1] + ' </a>'
   + '      <br/>'
   + '      X songs'
   + '      <br/>'
@@ -13,14 +14,21 @@ var collectionItemTemplate =
   + '  </div>'
   + '</div>'
   ;
-  window.onload = function() {
-    // #1
-    var collectionContainer = document.getElementsByClassName('album-covers')[0];
-    // #2
-    collectionContainer.innerHTML = '';
+        // #2
+      return $(template);
+  };
 
+ 
+ $(window).load(function() {
+    // #3
+    var $collectionContainer = $('.album-covers');
+    // #2
+    //collectionContainer.innerHTML = '';
+    $collectionContainer.empty();
     // #3
     for (var i = 0; i < 12; i++) {
-        collectionContainer.innerHTML += collectionItemTemplate;
+        //collectionContainer.innerHTML += collectionItemTemplate;
+        var $newThumbnail = buildCollectionItemTemplate(i);
+        $collectionContainer.append($newThumbnail);
     }
-}
+});
